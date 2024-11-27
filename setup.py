6b13d5789d391c754e39e4c208f1ad6826130877
Roman Martin <web.github@online-arts.de>
2024-11-27 16:11:10 +0100

    Add logo

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3-0004'
+VERSION = '0.1.0-03'
 DESCRIPTION = 'PySpiro'
-LONG_DESCRIPTION = 'Spirometry reference correction'
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
 
 setup(
         name="PySpiro",
         version=VERSION,
         author="Roman Martin, Hendrik Pott",
-        author_email="<roman.martin@hhu.de>, <hendrik.pott@uni-marburg.de>",
+        author_email="roman.martin@hhu.de, hendrik.pott@uni-marburg.de",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        package_data={'': ['data/gli_2012_splines.csv', 'gli_2012_coefficients.csv']},
+        package_data={'': ['data/gli_2012_splines.csv', 'data/gli_2012_coefficients.csv', 'data/PySpiro.png']},
         include_package_data=True,
         install_requires=["pandas", "numpy"],
         keywords=['python', 'respirology', 'spirometry'],
         classifiers= [
-            "Development Status :: 3 - Alpha",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Healthcare Industry",
+            "Intended Audience :: Science/Research",
             "Intended Audience :: Education",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: Implementation :: PyPy",
+            "Operating System :: MacOS",
             "Operating System :: Microsoft :: Windows",
+            "Operating System :: Unix",
         ]
 )
